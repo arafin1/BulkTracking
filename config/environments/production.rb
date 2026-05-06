a571d0df9df6db01e028024026be2a9da7880d11
Arafin <arafinsaadmahmud@gmail.com>
2026-05-06 16:15:39 +0600

    Fix secret key configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+    config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -83,8 +84,6 @@ Rails.application.configure do
   # ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } 
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
-  
 end
