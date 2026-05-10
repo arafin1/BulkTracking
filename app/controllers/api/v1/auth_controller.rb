aa7f274837ebdeebbf8c03b22b2221eada7d4bcd
Arafin <arafinsaadmahmud@gmail.com>
2026-05-10 19:37:26 +0600

    Fix auth controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -19,30 +19,28 @@ class Api::V1::AuthController < ApplicationController
       render json: user.errors, status: :unprocessable_entity
     end
   end
-  
-def login
-  email = params[:email] || params.dig(:auth, :email)
-  password = params[:password] || params.dig(:auth, :password)
-  
-  user = User.find_by(email: email)
-  
-  if user&.authenticate(password)
-    token = encode_token({ user_id: user.id })
-    render json: { 
-      message: "Login successful",
-      token: token,
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        role: user.role
+
+  def login
+    email = params[:email] || params.dig(:auth, :email)
+    password = params[:password] || params.dig(:auth, :password)
+    user = User.find_by(email: email)
+    if user&.authenticate(password)
+      token = encode_token({ user_id: user.id })
+      render json: { 
+        message: "Login successful",
+        token: token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role
+        }
       }
-    }
-  else
-    render json: { error: "Invalid email or password" }, 
-           status: :unauthorized
+    else
+      render json: { error: "Invalid email or password" }, 
+             status: :unauthorized
+    end
   end
-end
 
   private
 
